9f26e97b01a7dad09aa117cfac6daeb9e808c7a0
taddbackus <104407802+taddbackus@users.noreply.github.com>
2022-09-26 01:01:51 -0400

    Working on first MLR model

--- EDAv1.R
+++ EDAv1.R
@@ -1,8 +1,9 @@
 library(ggplot2)
 library(GGally)
 library(tidyverse)
-
-
+library(lindia)
+library(olsrr)
+theme_set(theme_minimal())
 
 carDataRaw = read.csv('/Users/taddbackus/School/fall22/appliedStat/Project1/data1.csv',header=TRUE)
 carData = carDataRaw
@@ -21,6 +22,14 @@ carData = carData %>%
     highwayMPG = highway.MPG,
     cityMPG = city.mpg,
   )
+
+# Dealing with correlation between MPGS
+# For gasoline vehicles, the label shows City, Highway, and Combined MPG (miles per gallon) values. 
+#The Combined MPG value is the most prominent for the purpose of quick and easy comparison across vehicles. 
+#Combined fuel economy is a weighted average of City and Highway MPG values that is calculated by weighting the City value by 55% and the Highway value by 45%.
+# -- epa.gov
+cor(carData$highwayMPG,carData$cityMPG)
+carData$combMPG = carData$highwayMPG*.45 + carData$cityMPG*.55
 
 # Chaning characters to factors
 carData$Make = as.factor(carData$Make)
@@ -108,6 +117,7 @@ carData$horsepower[is.na(carData$horsepower) &
                      )
 carDataNA = carData[is.na(carData$horsepower),]
 carDataClean = na.omit(carData)
+carDataClean = subset(carDataClean,select=-c(highwayMPG,cityMPG))
 sapply(carDataClean, function(x) sum(is.na(x)))
 
 
@@ -120,17 +130,103 @@ ggpairs(carNum)
 
 # Transforming MSRP and MPG
 carDataTrans = carDataClean
-carDataTrans$highwayMPG = log(carDataTrans$highwayMPG)
-carDataTrans$cityMPG = log(carDataTrans$cityMPG)
 carDataTrans$MSRP = log(carDataTrans$MSRP)
+carDataTrans$combMPG = log(carDataTrans$combMPG)
 carNumTrans = carDataTrans[,num_cols]
 ggpairs(carNumTrans)
 
 # Splitting into 80 10 10
-ss = sample(1:3, size=nrow(carDataRaw),replace=TRUE,prob=c(0.8,0.1,0.1))
-train = carDataRaw[ss==1,]
-test = carDataRaw[ss==2,]
-val = carDataRaw[ss==3,]
+ss = sample(1:3, size=nrow(carDataClean),replace=TRUE,prob=c(0.8,0.1,0.1))
+trainC = carDataClean[ss==1,]
+trainT = carDataTrans[ss==1,]
+testT = carDataTrans[ss==2,]
+valT = carDataTrans[ss==3,]
+
+# Objective 1 model
+# Variables i think are important/simple
+partialData = subset(trainT,
+                     select=c(
+                       combMPG,
+                       cylinders,
+                       horsepower,
+                       Year,
+                       doors,
+                       Make,
+                       fuelType,
+                       transmission,
+                       drive,
+                       MSRP
+                     ))
+partFit1 = lm(MSRP~.,data=partialData)
+summary(partFit1)
+gg_diagnose(partFit1)
+
+partFitSelect = ols_step_both_aic(partFit1,details=TRUE)
+
+# All of the variables
+fullFit1 = lm(MSRP~.,data=trainT)
+summary(fullFit1)
+fullFitSelect = ols_step_both_aic(fullFit1,details=TRUE)
+summary(fullFitSelect)
+
+# Final? objective 1 fit
+obj1Fit = lm(MSRP~combMPG+cylinders+horsepower+Year+doors+Make+fuelType+transmission+drive+Popularity,data=trainT)
+summary(obj1Fit)
+plot(obj1Fit$residuals)
+
+# Playing with iteraction terms
+obj1Fit2 = lm(MSRP~combMPG+cylinders+horsepower+Year+doors+Make+fuelType+transmission+
+                drive+Popularity+category+horsepower:category,data=trainT)
+summary(obj1Fit2)
+AIC(obj1Fit2)
+summary(lm(MSRP~combMPG+fuelType+combMPG:fuelType,data=trainT))
+AIC(lm(MSRP~horsepower+category+horsepower:category,data=trainT))
+AIC(obj1Fit)
 
 
+# EDA
+ggplot(carNumTrans,aes(x=MSRP,y=as.factor(doors)))+
+  geom_boxplot()
 
+ggplot(carDataTrans,aes(x=MSRP,y=style))+
+  geom_boxplot()
+
+ggplot(carDataTrans,aes(x=Popularity))+
+  geom_histogram()
+ggplot(trainC, aes(x=MSRP))+
+  geom_histogram(bins=100)+
+  xlim(0,25000)
+ggplot(trainT, aes(x=MSRP))+
+  geom_histogram(bins=100)
+
+ggplot(trainT, aes(x=Popularity,y=MSRP))+
+  geom_point(position='jitter')+
+  geom_smooth(method='lm')
+
+popMake = trainT %>%
+  group_by(Make) %>%
+  summarize(avgCost = mean(MSRP))
+popMake$avgPop = trainT %>%
+  group_by(Make) %>%
+  summarize(avgPop = mean(Popularity))
+ggplot(popMake,aes(x=avgPop$avgPop,y=avgCost,color=Make))+
+  geom_point()
+
+ggplot(trainT,aes(x=horsepower,y=MSRP,color=drive))+
+  geom_point()
+
+ggplot(trainC,aes(x=MSRP,y=category))+
+  geom_boxplot()
+summary(trainC$category)
+
+ggplot(trainT,aes(x=MSRP,y=style))+
+  geom_boxplot()
+
+ggplot(trainT,aes(x=horsepower,y=MSRP,color=category))+
+  geom_smooth(method='lm',show.legend = FALSE)
+
+ggplot(trainT,aes(x=combMPG,y=MSRP,color=category))+
+  geom_smooth(method='lm',show.legend=FALSE)
+
+ggplot(trainT,aes(x=combMPG,y=MSRP,color=fuelType))+
+  geom_smooth(method='lm',show.legend=FALSE)
